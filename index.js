8069a9fb4178e4938b5f644f4bc73bac6741d176
Rory Goddard <goddardrory@gmail.com>
2024-08-07 13:32:28 +0200

    feat: Add author name to web page

--- index.js
+++ index.js
@@ -15,8 +15,16 @@
  * Instructions for this were found on CSS Tricks: 
  * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
  */
+const authorNameHeaderEl = document.getElementById("image-author")
 
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(response => response.json())
-    .then(data => document.body.style.backgroundImage = `url(${data.urls.full})`)
+    .then(data => setBackgroundImage(data))
+    .catch(error => console.error(`Error fetching image: ${error}`))
 
+function setBackgroundImage(obj) {
+    document.body.style.backgroundImage = `url(${obj.urls.full})`
+    authorNameHeaderEl.textContent = obj.user.name
+}
+// author is data.user.name
+// 
