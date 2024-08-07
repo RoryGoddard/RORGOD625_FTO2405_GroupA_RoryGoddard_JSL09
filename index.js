25297639289e91b4c3f3faadc909c80f1cdd049a
Rory Goddard <goddardrory@gmail.com>
2024-08-07 16:49:44 +0200

    fix: Refactor functions to use async await

--- index.js
+++ index.js
@@ -6,63 +6,39 @@ setInterval(getCurrentTime, 1000)
 
 const authorNameHeaderEl = document.getElementById("image-author")
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(response => response.json())
-    .then(data => setBackgroundImage(data))
-    .catch(err => {
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
-        authorNameHeaderEl.textContent = `By: Dodi Achmad`
-        console.error(`Error fetching image: ${err}`)
-        }
-    )
+const resBackground = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+const dataBackground = await resBackground.json()
+document.body.style.backgroundImage = `url(${dataBackground.urls.full})`
+authorNameHeaderEl.textContent = `By: ${dataBackground.user.name}`
 
-function setBackgroundImage(obj) {
-    document.body.style.backgroundImage = `url(${obj.urls.full})`
-    authorNameHeaderEl.textContent = `By: ${obj.user.name}`
+
+const resCoin = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+if (!resCoin.ok) {
+    throw Error(`Something went wrong. Status Code: ${resCoin.status}`)
 }
+const coinData = await resCoin.json()
+document.getElementById("crypto-top").innerHTML = `
+<img src=${coinData.image.small} />
+<span>${coinData.name}</span>
+`
+document.getElementById("crypto-prices").innerHTML = `
+    <p>🎯: R${coinData.market_data.current_price.zar.toLocaleString()}</p>
+    <p>👆: R${coinData.market_data.high_24h.zar.toLocaleString()}</p>
+    <p>👇: R${coinData.market_data.low_24h.zar.toLocaleString()}</p>
+`
 
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
-    .then(response => {
-        if (!response.ok) {
-            throw Error(`Something went wrong. Status Code: ${response.status}`)
-        }
-        return response.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
-        <img src=${data.image.small} />
-        <span>${data.name}</span>
-    `
-        document.getElementById("crypto-prices").innerHTML = `
-            <p>🎯: R${data.market_data.current_price.zar.toLocaleString()}</p>
-            <p>👆: R${data.market_data.high_24h.zar.toLocaleString()}</p>
-            <p>👇: R${data.market_data.low_24h.zar.toLocaleString()}</p>
-        `
+navigator.geolocation.getCurrentPosition(async position => {
+    const resWeather = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+    if (!resWeather.ok) {
+        throw Error(`Weather data not available. Status Code: ${resWeather.status}`)
     }
-    )
-    .catch(err => {
-        console.error(`Error fetching coin data: ${err}`)
-        }
-    )
-
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
-    .then(res => {
-        if (!res.ok) {
-            throw Error(`Weather data not available. Status Code: ${res.status}`)
-        }
-        return res.json()
-    })
-    .then(data => {
-        console.log(data)
-        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        document.getElementById("weather").innerHTML = `
-            <img src=${iconUrl} />
-            <p>${Math.trunc(data.main.temp)}°</p>
-            <p class="weather-city">${data.name}</p>
-        `
-    })
-    .catch(err => console.error(err))
-  });
+    const dataWeather = await resWeather.json()
+    const iconUrl = `http://openweathermap.org/img/wn/${dataWeather.weather[0].icon}@2x.png`
+    document.getElementById("weather").innerHTML = `
+        <img src=${iconUrl} />
+        <p>${Math.trunc(dataWeather.main.temp)}°</p>
+        <p class="weather-city">${dataWeather.name}</p>
+    `
+    });
 
 
